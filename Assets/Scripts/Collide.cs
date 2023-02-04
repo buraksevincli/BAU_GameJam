f53dc745c2d9f03f89b8916be48142cd9b5b3d2a
Burak Sevincli <brksvncli@gmail.com>
2023-02-04 20:11:21 +0300

    Level 2 Design Created

--- Assets/Scripts/Collide.cs
+++ Assets/Scripts/Collide.cs
@@ -6,18 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class Collide : MonoBehaviour
 {
-    [SerializeField] private GameObject _explosion;
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Tutorial"))
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
-        }
-
-        if (col.gameObject.CompareTag("Enemy"))
-        {
-            col.gameObject.SetActive(false);
-            _explosion.SetActive(true);
         }
     }
 }
