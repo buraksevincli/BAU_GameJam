5f261a04026a79269170f1c689f03b379d56aa28
Burak Sevincli <brksvncli@gmail.com>
2023-02-04 14:46:11 +0300

    Explosion Anim

--- Assets/Scripts/Collide.cs
+++ Assets/Scripts/Collide.cs
@@ -6,11 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class Collide : MonoBehaviour
 {
+    [SerializeField] private GameObject _explosion;
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Tutorial"))
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+        }
+
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            col.gameObject.SetActive(false);
+            _explosion.SetActive(true);
         }
     }
 }
