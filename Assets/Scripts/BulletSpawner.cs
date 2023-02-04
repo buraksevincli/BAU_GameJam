070c83979a97148fef6664143c4e67fc9c6572f2
Burak Sevincli <brksvncli@gmail.com>
2023-02-04 13:41:30 +0300

    Bullet Spawner & Anim Added

---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class BulletSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _bullet;
+    private GameObject instantiatedBullet;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            Invoke(nameof(Spawn),0.1f);
+        }
+    }
+
+    private void Spawn()
+    {
+        instantiatedBullet = Instantiate(_bullet, transform.position, Quaternion.identity);
+        Destroy(instantiatedBullet,5f);
+    }
+}
