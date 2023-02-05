bf73f9b97d8b7a0642b945269d27c7a30c905064
Onur Demircan <onurdemircan438@gmail.com>
2023-02-05 04:45:14 +0300

    PoisonBubble Speed Fixed

--- Assets/Scripts/PoisonPlant/PoisonBubbleForce.cs
+++ Assets/Scripts/PoisonPlant/PoisonBubbleForce.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PoisonBubbleForce : MonoBehaviour
 {
+    [SerializeField] float downForce;
+    [SerializeField] float leftForce;
+
     Rigidbody2D rb2d;
 
     // Start is called before the first frame update
@@ -11,11 +14,11 @@ public class PoisonBubbleForce : MonoBehaviour
     {
         rb2d = GetComponent<Rigidbody2D>();
 
-        float randNumber = Random.Range(100, 150);
+        //float randNumber = Random.Range(100, 150);
         //float randNumber2 = Random.Range(50, 200);
 
-        rb2d.AddForce(-transform.up * randNumber);
-        rb2d.AddForce(-transform.right * 250);
+        rb2d.AddForce(-transform.up * downForce);
+        rb2d.AddForce(-transform.right * leftForce);
     }
 
     // Update is called once per frame
